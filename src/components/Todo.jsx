8fb4f7361b751564607d98cc3d93ba789dae5588
Priya Kushawaha <priyakushawaha73@gmail.com>
2024-06-18 17:54:20 +0545

    Todo webapp

---
+++ src/components/Todo.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+import "../assets/css/todo.css";
+const Todo = () =>{
+    return(
+        <div>
+             <h1>ToDo WebApp List</h1>
+        
+        </div>
+    );
+};
+export default Todo;
