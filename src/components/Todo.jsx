cf6c27cb65b68163465f799fef46524c515ad428
Priya Kushawaha <priyakushawaha73@gmail.com>
2024-06-18 18:17:58 +0545

    button added

--- src/components/Todo.jsx
+++ src/components/Todo.jsx
@@ -4,7 +4,8 @@ const Todo = () =>{
     return(
         <div>
              <h1>ToDo WebApp List</h1>
-        
+            <input type="text" id="task" placeholder="Enter a new task"></input>
+            <button>Add</button>
         </div>
     );
 };
