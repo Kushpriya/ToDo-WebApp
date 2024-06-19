fb6988ed9d1f5b7eee6b484bacda0690dc8c221d
Priya Kushawaha <priyakushawaha73@gmail.com>
2024-06-19 13:56:51 +0545

    add updated

--- src/components/Todo.jsx
+++ src/components/Todo.jsx
@@ -1,11 +1,32 @@
-import React from "react";
+import React, { useState } from "react";
 import "../assets/css/todo.css";
+
 const Todo = () =>{
+    const [value,setValue] = useState();
+    const [todo,setTodo] = useState([]);
+    const handleClick = () =>{
+        if(value !== "" && todo !== ""){
+            setTodo([...todo,value]);
+            setValue('');
+        }
+    };
     return(
         <div>
              <h1>ToDo WebApp List</h1>
-            <input type="text" id="task" placeholder="Enter a new task"></input>
-            <button>Add</button>
+           <div className="head">
+           {/* <input type="text" id="task" placeholder="Enter a new task"/> */}
+                <input onChange={e => setValue(e.target.value)} value={value}
+                id="task" placeholder="Enter a new task"/>
+                <button onClick={handleClick}>Add</button>
+           </div>
+           
+           <div>
+            <ul>
+                {todo.map((todos) =>(
+                    <li>{todos}</li>
+                ))}
+            </ul>
+           </div>
         </div>
     );
 };
